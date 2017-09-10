f305bb07be1afec1b92d1e742f44e71a4932d28b
Maple27 <654686099@qq.com>
2017-09-10 13:28:05 +0800

    更新源代码

--- Sudoku/sudoku.cpp
+++ Sudoku/sudoku.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include <stdio.h>  
 #include <time.h>  
 #include <stdlib.h> 
@@ -9,13 +8,13 @@
 */
 
 int sudoku[9][9];
-FILE *fp;
+FILE *fp; 
 
 /*检查数独棋盘内数字是否符合规则（即同行同列同九宫格内没有重复数字）*/
-int check(int sudoku[][9], int i, int j) {
-	int p, q, m, n;
+int check(int sudoku[][9], int i, int j){
+	int p,q,m,n;
 	int t = sudoku[i][j];
-
+	
 	//横向检查
 	for (p = 0; p<9; p++)
 		if (p != i && sudoku[p][j] == t)
@@ -25,7 +24,7 @@ int check(int sudoku[][9], int i, int j) {
 		if (p != j && sudoku[i][p] == t)
 			return 0;
 
-	//九宫格内检查 
+    //九宫格内检查 
 	p = i / 3;
 	q = j / 3;
 	for (m = p * 3; m<p * 3 + 3; m++)
@@ -38,47 +37,47 @@ int check(int sudoku[][9], int i, int j) {
 }
 
 /*输出9*9的数独棋盘*/
-void sudoku_print(int sudoku[][9]) {
-	int i, j;
-	for (i = 0; i < 9; i++) {
-		printf("%4d", sudoku[i][0]);
-		fprintf(fp, "%d", sudoku[i][0]);
-		for (j = 1; j < 9; j++) {
-			printf("%4d", sudoku[i][j]);
-			fprintf(fp, " %d", sudoku[i][j]);
+void sudoku_print(int sudoku[][9]){
+	int i, j; 
+	for (i = 0; i < 9; i++){
+		printf("%4d", sudoku[i][0]);  
+		fprintf(fp,"%d",sudoku[i][0]);  
+		for (j = 1; j < 9; j++){
+			printf("%4d", sudoku[i][j]);  
+    		fprintf(fp," %d",sudoku[i][j]);  
 		}
-		fprintf(fp, "\n");
+		fprintf(fp,"\n");  
 		printf("\n");
 	}
 }
 
 /*用回溯法获取N个数独棋盘*/
-int getSudoku(int N) {
+int getSudoku(int N){
 	int k = 0, num = 0;
-	fp = fopen("sudoku.txt", "w");
-	while (1) {
+	fp=fopen("sudoku.txt","w");
+	while (1){
 		int i = k / 9;
 		int j = k % 9;
-		while (1) {
+		while (1){
 			sudoku[i][j]++;
-			if (sudoku[i][j] == 10) {
+			if (sudoku[i][j] == 10){
 				sudoku[i][j] = 0;
 				k--;
 				break;
 			}
-			else if (check(sudoku, i, j) == 1) {
+			else if (check(sudoku, i, j) == 1){
 				k++;
 				break;
 			}
 		}
 
-		if (k == 81) {
-			printf("第%d个数独棋盘: \n", num++);
-			fp = fopen("sudoku.txt", "a");
+		if (k == 81){
+			printf("第%d个数独棋盘: \n", ++num);
+			fp=fopen("sudoku.txt","a");
 			sudoku_print(sudoku);
-			fprintf(fp, "\n");
-			if (num >= N) {
-				printf("over\n");
+			fprintf(fp,"\n");  
+			if (num >= N){
+				printf("over!\n");
 				fclose(fp);
 				return 0;
 			}
@@ -88,18 +87,37 @@ int getSudoku(int N) {
 
 }
 
-int main() {
+int main(int argc, char ** argv){
 	int i, j, t = 0, r = 1;
-	int N;
-	scanf("%d", &N);
+	char* arg1 = argv[1]; 
+	int N = atoi(argv[2]);
+	//错误处理 
+	if(!(arg1[0] == '-' && arg1[1] == 'c')){
+		printf("输入有误，请重新运行.\n");
+		return 0;
+	}
+	if(N == 0){
+		printf("输入有误，请重新运行.\n");
+		return 0;
+	}
+	/*while(1){
+			int l = scanf("%d",&N);
+			if(l == 0){
+				printf("输入有误，请重新输入.\n");
+				fflush(stdin);
+				continue;
+			} else {
+				break;
+			}
+	}*/
 	//用当前时间使rand()获取的随机数更具有随机性 
 	srand(time(0));
 	for (i = 0; i < 9; i++)
 		for (j = 0; j < 9; j++)
 			sudoku[i][j] = 0;
-	for (i = 0; i < 9; i++) {
-		sudoku[0][0] = (1 + 0) % 9;
-		t = (rand()*rand()) % 81;
+	for (i = 0; i < 9; i++){
+		sudoku[0][0]=(1+0)% 9;
+		t = (rand() + r) % 81;
 		sudoku[t / 9][t % 9] = i + 1;
 	}
 	getSudoku(N);
